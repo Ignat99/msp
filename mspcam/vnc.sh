377312876f0277ee50a27e1064b9572d4a77f026
Ignat Ignatov <ignat99@gmail.com>
2015-04-24 21:48:17 +0300

    Update vnc.sh
    Add links to APK and PKG

--- mspcam/vnc.sh
+++ mspcam/vnc.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+# http://elinux.org/RPi_VNC_Server
+# sudo apt-get install tightvncserver
+# https://www.raspberrypi.org/documentation/remote-access/vnc/mac.md
+# VNC View for Mac http://www.realvnc.com/download/vnc/latest
 vncserver :1 -geometry 1200x700 -depth 16
 

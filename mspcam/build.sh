c0942163e1516268787ac2d25700194d2d63e047
root <root@odroid-wheezy.(none)>
2015-05-10 16:17:17 +0200

    cams _0123

--- mspcam/build.sh
+++ mspcam/build.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 filename=$(date --rfc-3339=date)
+port=8080
 
 i=0
-for f in `ls -tr /mnt/dav/out/$filename/*.jpg 2>/dev/null`
+for f in `ls -tr /mnt/dav/out/$filename/$port/*.jpg 2>/dev/null`
 do
   newf=`printf %06d $i`.jpg
   echo $f "-->" $newf
   mv $f $newf
   i=$((i+1))
 done
-rmdir -R /mnt/dav/out/$filename/
-avconv -r 10 -i %06d.jpg -r 10 -vcodec mjpeg -qscale 1   /mnt/dav/$filename.avi
+rmdir /mnt/dav/out/$filename/$port/
+avconv -r 10 -i %06d.jpg -r 10 -vcodec mjpeg -qscale 1   /mnt/dav/$port$filename.avi
 rm *.jpg
 
--- mspcam/cam.sh
+++ mspcam/cam.sh
@@ -9,17 +9,23 @@
 
 filename=$(perl -e "print time")
 foldername=$(date --rfc-3339=date)
+port=8080
+portfilename=$port$filename
 
 #printf "$foldername"
 
-curl -u artak:123 http://localhost:8080/?action=snapshot > $filename
+curl -u artak:123 http://localhost:$port/?action=snapshot > $portfilename
 timestamp=`stat -c %y $filename`
 
 if [ ! -f /mnt/dav/out/$foldername ]; then
   mkdir /mnt/dav/out/$foldername
 fi
 
-convert $filename -fill black -fill white -pointsize 15 -draw  "text 5,15 '${timestamp:0:19}'" /mnt/dav/out/$foldername/$filename.jpg
+if [ ! -f /mnt/dav/out/$foldername/$port ]; then
+  mkdir /mnt/dav/out/$foldername/$port
+fi
 
-rm $filename
+convert $portfilename -fill black -fill white -pointsize 15 -draw  "text 5,15 '${timestamp:0:19}'" /mnt/dav/out/$foldername/$port/$filename.jpg
+
+rm $portfilename
 
